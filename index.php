11dc7a5e12a9941dea365ae7949dcaacc7e061f9
Joabe Kachorroski <kernelkill@outlook.com>
2019-09-01 20:40:12 -0400

    Criando um sistema de paginação

---
+++ index.php
@@ -0,0 +1,37 @@
+
+<?php
+require("cabecalho.php")
+
+?>
+		
+		<div class="base-home">
+			<h1 class="titulo"><span class="cor">SEJA</span> BEM VINDO</h1>
+		<div class="base-colunas">	
+			<a href="frm_cadastro.html" class="col">
+				<i class="icone ico1"></i>
+				<span><b>NOVO</b> CADASTRO</span>
+			</a>	
+			
+			<a href="lst_contatos.html" class="col">
+				<i class="icone ico2"></i>
+				<span><b>lista de</b> contatos</span>
+			</a>
+			
+			<a href="frm_cadastro.html" class="col">
+				<i class="icone ico3"></i>
+				<span><b>Editar</b> contatos</span>
+			</a>
+			
+			<a href="frm_cadastro.html" class="col">
+				<i class="icone ico4"></i>
+				<span><b>Excluir</b> contatos</span>
+			</a>
+		</div>	
+</div>	
+		
+		<div class="base-rodape">
+		<p>DIREITOS RESERVADOS SYSTEM32</p>
+</div>	</div>		
+</div>		
+</body>
+</html>
---
+++ rodape.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+<meta charset="utf-8">
+<title>Paginação</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+<div class="conteudo">	
+	<div class="base-central">	
+		<div class="base-topo">
+			<a href="index.php?link=1" class="logo"></a>
+			<div class="base-busca">
+				<form action="">
+					<input type="text" placeholder="Pesquisa">
+					<input type="submit" value="" class="but">
+				</form>				
+			</div>
+		</div>
+		
+		<nav class="menu">
+	<ul>
+		<li><a href="index.html">Home</a></li>
+		<li><a href="frm_cadastro.html">Cadastrar novo</a></li>
+		<li><a href="lst_contatos.html">Lista de contatos</a></li>
+		<li><a href="frm_cadastro.html">Editar</a></li>
+		<li><a href="frm_cadastro.html">Exluir</a></li>
+	</ul>
+</nav>	
