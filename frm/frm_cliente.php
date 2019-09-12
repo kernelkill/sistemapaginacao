ffdcf2f6bf23b4b1166d16ceb19c322b82014075
Joabe Kachorroski <kernelkill@outlook.com>
2019-09-12 00:47:30 -0400

    arquivo de operação programada e testada, a mesma ja esta fazendo a inserção dos dados via função.

--- frm/frm_cliente.php
+++ frm/frm_cliente.php
@@ -3,7 +3,7 @@
 		<div class="base-home">
 			<h1 class="titulo"><span class="cor">Novo</span> cadastro</h1>
 		<div class="base-formulario">	
-			<form action="../op/op_cliente.php" method="POST">
+			<form action="op/op_cliente.php" method="POST">
 				<label>Nome</label>
 					<input name="txt_nome" value="" type="text" placeholder="Insira umm nome">
 				<label>Email</label>
@@ -23,17 +23,14 @@
 				<div class="col">
 					<label>CEP</label>
 					<input name="txt_cep" value="" type="text" placeholder="Insira seu CEP">
-				</div>	
-				
-				<div class="col">
-					<label>CPF</label>
-					<input name="txt_cpf" value="" type="text" placeholder="Insira seu CPF">
 				</div>
 				
+				<div class="col">
 				<input type="hidden" name="acao" value="Cadastrar">
 				<input type="hidden" name="id" value="">
 				<input type="submit" value="Cadastrar" class="btn">
 				<input type="reset" name="Reset" id="button" value="Limpar" class="btn limpar">
+				</div>
 			</form>
 		</div>	
 </div>	
--- op/op_cliente.php
+++ op/op_cliente.php
@@ -5,3 +5,31 @@
  * Date: 11/09/19
  * Time: 23:50
  */
+
+ require("../config/config.php");
+ require("../config/crud.php");
+
+
+ $cliente   = strip_tags($_POST["txt_nome"]);
+ $email     = strip_tags($_POST["txt_email"]);
+ $endereco  = strip_tags($_POST["txt_endereco"]);
+ $fone      = strip_tags($_POST["txt_fone"]);
+ $bairro    = strip_tags($_POST["txt_bairro"]);
+ $cep       = strip_tags($_POST["txt_cep"]);
+ //$cpf       = strip_tags($_POST["txt_cpf"]);
+
+$dados = array(
+    "cliente"   => $cliente,
+    "email"     => $email,
+    "endereco"  => $endereco,
+    "fone"      => $fone,
+    "bairro"    => $bairro,
+    "cep"       => $cep   
+);
+
+
+inserir("cliente",$dados);
+
+header("location:". URL_BASE ."index.php?link=3");
+
+ ?>
